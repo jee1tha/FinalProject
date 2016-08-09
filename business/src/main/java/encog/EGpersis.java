8e470e096e54c1392d8e155e64e526588021e522
VABAYJE <jeewantha.abayakoon@pearson.com>
2016-08-09 11:09:28 +0530

    new repo for pl

--- business/src/main/java/encog/EGpersis.java
+++ business/src/main/java/encog/EGpersis.java
@@ -8,6 +8,7 @@ import org.encog.ml.data.basic.BasicMLDataSet;
 import org.encog.ml.train.MLTrain;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.Train;
 import org.encog.neural.networks.training.propagation.Propagation;
 import org.encog.neural.networks.training.propagation.back.Backpropagation;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
@@ -19,6 +20,12 @@ public class EGpersis {
 			{ 0.0, 1.0 }, { 1.0, 1.0 } };
 	
 	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+	
+/*	public static double XOR_INPUT2[][] = { { 1.0, 1.0 }, { 1.0, 0.0 },
+			{ 1.1, 1.0 }, { 1.0, 1.0 } };
+	
+	public static double XOR_IDEAL2[][] = { { 1.0 }, { 1.0 }, { 1.0 }, { 1.0 } };*/
+	
 	public static final String FILENAME = "network.eg";
 	public static void main(String[] args) {
 		EGpersis g = new EGpersis();
@@ -42,10 +49,15 @@ public class EGpersis {
 		
 		MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
 		
-		final Backpropagation train =  new Backpropagation(network, trainingSet);
+		final Propagation  train =  new Backpropagation(network, trainingSet);
+		int epoch = 1;
 		do {
 			train.iteration();
-		} while (train.getError() > 0.009);
+			System.out.println("Epoch #" + epoch + 
+                    " Error:" + train.getError());
+					epoch++;
+
+		} while (train.getError() < 0.009);
 		
 		double e = network.calculateError(trainingSet);
 		System.out.println("Network trained to error :" + e);
@@ -60,9 +72,11 @@ public class EGpersis {
 		System.out.println("Loading Network");
 		BasicNetwork network = (BasicNetwork) EncogDirectoryPersistence.loadObject(new File(FILENAME));
 		
-		MLDataSet trainingSet2 = new BasicMLDataSet(XOR_INPUT,XOR_IDEAL);
+		BasicMLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT,XOR_IDEAL);
 		
-		double e = network.calculateError(trainingSet2);
+		double e = network.calculateError(trainingSet);
+		
+		System.out.println(network.compute((MLData) trainingSet));
 		System.out.println("Loaded network's error is (should be the same as above ):" + e);
 		
 	}
