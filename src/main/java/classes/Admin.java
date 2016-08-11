5335e2bdde4ecf94a1790dddc280afbb52c34218
VABAYJE <jeewantha.abayakoon@pearson.com>
2016-08-11 18:55:57 +0530

    deleted unnecessary files + added basic java classes

---
+++ src/main/java/classes/Admin.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class Admin extends Users {
+
+	private String empNo;
+	private String joinedDate;
+	
+	public String getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+	
+	public String getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(String joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+}
---
+++ src/main/java/classes/Applicants.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class Applicants extends Users{
+
+	private String nicNo;
+	private String birthDate;
+	
+	
+	public String getNicNo() {
+		return nicNo;
+	}
+	
+	public void setNicNo(String nicNo) {
+		this.nicNo = nicNo;
+	}
+	
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+}
